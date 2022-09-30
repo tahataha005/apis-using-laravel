1a305d9347b1c0f996ce70fda744bc0240db4960
tahataha005 <taha.f.taha0@gmail.com>
2022-09-30 23:58:41 +0300

    Creating Sorter function that sorts and input of random upper-lower case letters and numbers in FourFunctionsController

--- app/Http/Controllers/FourFunctions.php
+++ app/Http/Controllers/FourFunctions.php
@@ -4,7 +4,27 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class FourFunctions extends Controller
-{
-    //
+class FourFunctions extends Controller{
+    
+    function sorter($num){    
+
+        $value = 1; // Flag to flip between negative and postive number cases
+        $length = strlen(strval($num))-1; // length of the inputed number
+        $array = [];
+
+        if($num < 0){
+            $value = -1;
+            $num *= -1;
+            $length -= 1;
+        }
+
+        while($num !=0 ){
+            $part = (floor($num / 10**$length))*$value;
+            $num = $num%10**$length;
+            $array[] = $part*10**$length;
+            $length--;
+        }
+
+        return $array;
+    }
 }
