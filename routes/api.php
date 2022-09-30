5910814257d8a1051120d1f0e6bab2ff02c2a70a
tahataha005 <taha.f.taha0@gmail.com>
2022-10-01 00:19:01 +0300

    Creating a route for prefix function

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\Route;
 Route::get("/sorter/{num}",[FourFunctions::class,"sorter"]);
 Route::get("/binary/{string}",[FourFunctions::class,"binary"]);
 Route::get("/order/{string}",[FourFunctions::class,"order"]);
+Route::get("/operation/{string}",[FourFunctions::class,"prefix"]);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
