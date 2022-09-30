472bac9840bd889d3116c61bbafed523fdaea88d
tahataha005 <taha.f.taha0@gmail.com>
2022-10-01 00:08:11 +0300

    Creating a route for order function

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,9 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get("/sorter",[FourFunctions::class,"sorter"]);
+Route::get("/sorter/{num}",[FourFunctions::class,"sorter"]);
+Route::get("/binary/{string}",[FourFunctions::class,"binary"]);
+Route::get("/order/{string}",[FourFunctions::class,"order"]);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
